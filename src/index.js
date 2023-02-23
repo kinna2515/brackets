7f0707ca70d64923595572c8d56c4b32db0c596d
Inna Barysenka <inna.barysenka@gmail.com>
2023-02-23 18:28:19 +0300

    feat: solving brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  function getPairByOpeningBracket(bracketsConfig, bracket) {
+    for (let o = 0; o < bracketsConfig.length; o++) {
+      if (bracketsConfig[o][0] == bracket) {
+        return bracketsConfig[o];
+      }
+    }
+  }
+
+  function getPairByClosingBracket(bracketsConfig, bracket) {
+    for (let c = 0; c < bracketsConfig.length; c++) {
+      if (bracketsConfig[c][1] == bracket) {
+        return bracketsConfig[c];
+      }
+    }
+  }
+
+  let buffer = ''
+
+  for (let i = 0; i < str.length; i++) {
+    if (getPairByOpeningBracket(bracketsConfig, str[i]) != undefined && getPairByOpeningBracket(bracketsConfig, str[i])[0] == getPairByOpeningBracket(bracketsConfig, str[i])[1]) {
+      if (buffer.slice(-1) == getPairByOpeningBracket(bracketsConfig, str[i])[0]) {
+        buffer = buffer.slice(0, -1)
+      } else { buffer += str[i]}
+    }
+    else if (getPairByOpeningBracket(bracketsConfig, str[i]) != undefined) {
+      buffer += str[i]
+    }
+    else if (getPairByClosingBracket(bracketsConfig, str[i]) != undefined) {
+      if (buffer.slice(-1) != getPairByClosingBracket(bracketsConfig, str[i])[0]) {
+        return false;
+      }
+      else {
+        buffer = buffer.slice(0, -1)
+      }
+
+    }
+
+  }
+
+  return (buffer == '')
+
+
 }
